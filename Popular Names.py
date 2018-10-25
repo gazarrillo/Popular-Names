abbccf587ae7b7a8b67183bfd9e3506aeed690ad
Giovanni Zarrillo <giovannizarrillo@GAZ-MacBook-Air.local>
2018-10-24 23:26:42 -0400

    v2.0.0 Re-written Program to work with functions

--- Popular Names.py
+++ Popular Names.py
@@ -1,36 +1,37 @@
-#Initializing List
-boy_list = []
-girl_list = []
-#Opening File
-boy_file = open('BoyNames.txt', 'r')
-girl_file = open('GirlNames.txt','r')
-#Reading Files and Appending Lists
-for linefromfile in boy_file:
-    linefromfile = linefromfile.rstrip('\n')
-    boy_list.append(linefromfile)
-boy_file.close()
+def getNamesList(filename):
+    """This function reads a file and creates a list with its contents"""
+    filename_list = []
+    filename = open(filename, 'r')
+    for linefromfile in filename:
+         linefromfile = linefromfile.rstrip('\n')
+         filename_list.append(linefromfile)
+    filename.close()
+    return filename_list
 
-for linefromfile in girl_file:
-    linefromfile = linefromfile.rstrip('\n')
-    girl_list.append(linefromfile)
-girl_file.close()
-#Name Input
+def checkName(name, nameList):
+    """This function checks whether or not a name is in a list, it returns 0 if it's not im the list and its rank in the list if it is"""
+    x = 0
+    for i in range(0, len(nameList)):
+        if name == nameList[i]:
+            x = 1
+    if x == 0:
+        return 0
+    else:
+        return nameList.index(name) + 1
+
+boy_list = getNamesList('BoyNames.txt')
+girl_list = getNamesList('GirlNames.txt')
+
 name = input("Enter a person's name or 'stop' to stop: ")
-#Loop
+
 while name != 'stop':
-    boy = 0
-    girl = 0
-#Going through both name lists
-    for i in range(0, len(girl_list)):
-        if name == girl_list[i]:
-            print(name, "is a popular name and it's ranked: ", girl_list.index(name) + 1)
-            girl = 1
-        if name == boy_list[i]:
-            print(name, "is a popular name and it's ranked: ", boy_list.index(name) + 1)
-            boy = 1
-    if girl == 0:
-        print(name, "is not a popular girls name")
-    if boy == 0:
+    if checkName(name, boy_list) > 0:
+        print(name, "is a popular boys name and it's ranked: ", checkName(name, boy_list))
+    elif checkName(name, girl_list) > 0:
+        print(name, "is a popular girls name and it's ranked: ", checkName(name, girl_list))
+    if checkName(name, boy_list) == 0:
         print(name, "is not a popular boys name")
+    if checkName(name, girl_list) == 0:
+        print(name, "is not a popular girls name")
     print('')
     name = input("Enter a person's name or 'stop' to stop: ")
